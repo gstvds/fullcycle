4f1cff5d4519b0ef55808dafa53b1897cd2f1487
gstvds <gstvds@icloud.com>
2024-06-19 09:26:41 -0300

    feat: adds repository interface

---
+++ internal/events/domain/repository.go
@@ -0,0 +1,13 @@
+package domain
+
+type EventRepository interface {
+	ListEvents() ([]Event, error)
+	GetEventByID(eventID string) (*Event, error)
+	GetSpotsByEventID(eventID string) ([]*Spot, error)
+	GetSpotByName(eventID, spotName string) (*Spot, error)
+	// Creation will be added in the future
+	// CreateEvent(event *Event) error
+	// CreateSpot(spot *Spot) error
+	// CreateTicket(ticket *Ticket) error
+	ReserveSpot(spotID, ticketID string) error
+}
